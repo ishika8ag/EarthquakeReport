5de20797144c0e18fa8c97ab207491cc387b42e2
ishika8ag <agarwal8ishika@gmail.com>
2021-06-23 00:55:36 +0530

    quake commit

---
+++ app/src/main/java/com/example/quakereport/Earthquake.java
@@ -0,0 +1,40 @@
+package com.example.quakereport;
+//this class contains info of single earthquake
+public class Earthquake {
+    private double mMagnitude;
+    private String mLocation;
+    private Long mTimeInMilliseconds;
+    private String mUrl;
+
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
+    }
+
+    public double getmMagnitude()
+    {
+        return mMagnitude;
+    }
+    /**
+     * Returns the location of the earthquake.
+     */
+    public String getLocation() {
+        return mLocation;
+    }
+
+    /**
+     * Returns the time of the earthquake.
+     */
+    public long getTimeInMilliseconds() {
+        return mTimeInMilliseconds;
+    }
+
+    /**
+     * Returns the website URL to find more information about the earthquake.
+     */
+    public String getUrl() {
+        return mUrl;
+    }
+}
---
+++ app/src/main/java/com/example/quakereport/EarthquakeActivity.java
@@ -0,0 +1,64 @@
+package com.example.quakereport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class EarthquakeActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = EarthquakeActivity.class.getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.earthquake_activity);
+
+        // Create a fake list of earthquake locations.
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
+       /* final ArrayList<Earthquake> earthquakes=new extract<Earthquake>();
+        earthquakes.add(new Earthquake(22.5,"1",0.));
+        earthquakes.add("London");
+        earthquakes.add("Tokyo");
+        earthquakes.add("Mexico City");
+        earthquakes.add("Moscow");
+        earthquakes.add("Rio de Janeiro");
+        earthquakes.add("Paris");*/
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+
+        // Create a new adapter that takes the list of earthquakes as input
+        final EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        earthquakeListView.setAdapter(adapter);
+
+        // Set an item click listener on the ListView, which sends an intent to a web browser
+        // to open a website with more information about the selected earthquake.
+        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Find the current earthquake that was clicked on
+                Earthquake currentEarthquake = adapter.getItem(position);
+
+                // Convert the String URL into a URI object (to pass into the Intent constructor)
+                Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
+
+                // Create a new intent to view the earthquake URI
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+
+                // Send the intent to launch a new activity
+                startActivity(websiteIntent);
+            }
+        });
+    }
+
+}
